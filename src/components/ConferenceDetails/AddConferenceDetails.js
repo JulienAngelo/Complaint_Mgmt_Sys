7e398d13ab0a51bc5c95e8a141c07130930676d0
MiyuruWijesinghe <mkwijesinghe2016@gmail.com>
2021-06-30 10:24:34 +0530

    added dropdown for conference details

--- src/components/ConferenceDetails/AddConferenceDetails.js
+++ src/components/ConferenceDetails/AddConferenceDetails.js
@@ -1,25 +1,67 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import EditorSideNav from "../Navbar/EditorSideNav";
 import axios from "axios";
+import Select from "react-select";
 
 export default function AddConferenceDetails(props) {
 
-    const [data, setData] = useState({
-        conferenceId: "",
-        topic: "",
-        description: "",
-        conductor: "",
-        venue: "",
-        date: "",
-        startTime: "",
-        endTime: "",
-        payment: ""
-    })
+    const [conferenceList, setConferenceList] = useState([]);
+    const [optionsList, setOptionsList] = useState([]);
+    const [conferenceId, setConferenceId] = useState("");
+    const [topic, setTopic] = useState("");
+    const [description, setDescription] = useState("");
+    const [conductor, setConductor] = useState("");
+    const [venue, setVenue] = useState("");
+    const [date, setDate] = useState("");
+    const [startTime, setStartTime] = useState("");
+    const [endTime, setEndTime] = useState("");
+    const [payment, setPayment] = useState("");
+
+    useEffect(() => {
+        getConference();
+    }, [])
+
+    function getConference() {
+        axios.get("https://icaf-backend.herokuapp.com/conference/status/ACTIVE").then((res) => {
+            setConferenceList(res.data);
+        }).catch((err) => {
+            alert(err);
+        })
+    }
+
+    useEffect(() => {
+        if(conferenceList.length > 0) {
+            setOptionValues();
+        }
+    }, [conferenceList])
+
+    function setOptionValues() {
+        const gotOptions = conferenceList.map((conference, index) => ({
+            value : conference.id,
+            label : conference.name
+        }))
+        setOptionsList(gotOptions)
+    }
+
+    function onSelect(e) {
+        setConferenceId(e.value);
+    }
 
     function submit(e) {
         e.preventDefault();
-        axios.post("https://icaf-backend.herokuapp.com/conference-details/save", data).then((res) => {
-            console.log(data);
+        const dataObject = {
+            conferenceId,
+            topic,
+            description,
+            conductor,
+            venue,
+            date,
+            startTime,
+            endTime,
+            payment
+        }
+        axios.post("https://icaf-backend.herokuapp.com/conference-details/save", dataObject).then((res) => {
+            console.log(dataObject);
             alert(res.data.messages);
             props.history.push("/conference-details");
         }).catch((err) => {
@@ -47,12 +89,6 @@ export default function AddConferenceDetails(props) {
         })
     }
 
-    function handle(e) {
-        const newData = {...data}
-        newData[e.target.id] = e.target.value
-        setData(newData)
-    }
-
     return(
         <div className="main">
             <EditorSideNav />
@@ -73,55 +109,55 @@ export default function AddConferenceDetails(props) {
                             <div className="form-group row">
                                 <label htmlFor="conferenceId" className="col-sm-3">Conference</label>
                                 <div className="col-sm-5">
-                                    <input type="text" onChange={(e) => handle(e)} className="form-control" id="conferenceId" placeholder="Enter Conference" value={data.conferenceId} required/>
+                                    <Select options={optionsList} onChange={(e) => onSelect(e)} id="conferenceId" placeholder="Select Conference" single autoFocus isSearchable/>
                                 </div>
                             </div><br/>
                             <div className="form-group row">
                                 <label htmlFor="topic" className="col-sm-3">Topic</label>
                                 <div className="col-sm-5">
-                                    <input type="text" onChange={(e) => handle(e)} className="form-control" id="topic" placeholder="Enter Topic" value={data.topic} required/>
+                                    <input type="text" onChange={(e) => setTopic(e.target.value)} className="form-control" id="topic" placeholder="Enter Topic" required/>
                                 </div>
                             </div><br/>
                             <div className="form-group row">
                                 <label htmlFor="description" className="col-sm-3">Description</label>
                                 <div className="col-sm-5">
-                                    <textarea onChange={(e) => handle(e)} className="form-control" id="description" cols="30" rows="6" placeholder="Enter Description" value={data.description} required/>
+                                    <textarea onChange={(e) => setDescription(e.target.value)} className="form-control" id="description" cols="30" rows="6" placeholder="Enter Description" required/>
                                 </div>
                             </div><br/>
                             <div className="form-group row">
                                 <label htmlFor="conductor" className="col-sm-3">Conductor</label>
                                 <div className="col-sm-5">
-                                    <input type="text" onChange={(e) => handle(e)} className="form-control" id="conductor" placeholder="Enter Conductor" value={data.conductor} required/>
+                                    <input type="text" onChange={(e) => setConductor(e.target.value)} className="form-control" id="conductor" placeholder="Enter Conductor" required/>
                                 </div>
                             </div><br/>
                             <div className="form-group row">
                                 <label htmlFor="venue" className="col-sm-3">Venue</label>
                                 <div className="col-sm-5">
-                                    <input type="text" onChange={(e) => handle(e)} className="form-control" id="venue" placeholder="Enter Venue" value={data.venue} required/>
+                                    <input type="text" onChange={(e) => setVenue(e.target.value)} className="form-control" id="venue" placeholder="Enter Venue" required/>
                                 </div>
                             </div><br/>
                             <div className="form-group row">
                                 <label htmlFor="date" className="col-sm-3">Date</label>
                                 <div className="col-sm-5">
-                                    <input type="text" onChange={(e) => handle(e)} className="form-control" id="date" placeholder="Enter Date" value={data.date} required/>
+                                    <input type="text" onChange={(e) => setDate(e.target.value)} className="form-control" id="date" placeholder="Enter Date" required/>
                                 </div>
                             </div><br/>
                             <div className="form-group row">
                                 <label htmlFor="startTime" className="col-sm-3">Start Time</label>
                                 <div className="col-sm-5">
-                                    <input type="text" onChange={(e) => handle(e)} className="form-control" id="startTime" placeholder="Enter Start Time" value={data.startTime} required/>
+                                    <input type="text" onChange={(e) => setStartTime(e.target.value)} className="form-control" id="startTime" placeholder="Enter Start Time" required/>
                                 </div>
                             </div><br/>
                             <div className="form-group row">
                                 <label htmlFor="endTime" className="col-sm-3">End Time</label>
                                 <div className="col-sm-5">
-                                    <input type="text" onChange={(e) => handle(e)} className="form-control" id="endTime" placeholder="Enter End Time" value={data.endTime} required/>
+                                    <input type="text" onChange={(e) => setEndTime(e.target.value)} className="form-control" id="endTime" placeholder="Enter End Time" required/>
                                 </div>
                             </div><br/>
                             <div className="form-group row">
                                 <label htmlFor="payment" className="col-sm-3">Payment</label>
                                 <div className="col-sm-5">
-                                    <input type="text" onChange={(e) => handle(e)} className="form-control" id="payment" placeholder="Enter Payment" value={data.payment} required/>
+                                    <input type="text" onChange={(e) => setPayment(e.target.value)} className="form-control" id="payment" placeholder="Enter Payment" required/>
                                 </div>
                             </div><br/>
                             <button type="submit" className="btn btn-primary">Save</button>
